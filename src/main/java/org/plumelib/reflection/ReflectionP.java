d73ac9c7855e64c649995fae5c060495fc24533e
Michael Ernst <mernst@cs.washington.edu>
2026-07-23 16:14:00 -0700

    Copy-edit

--- src/main/java/org/plumelib/reflection/ReflectionP.java
+++ src/main/java/org/plumelib/reflection/ReflectionP.java
@@ -42,7 +42,7 @@ public final class ReflectionP {
 
   /**
    * Returns true iff sub is a subtype of sup. If sub == sup, then sub is considered a subtype of
-   * sub and this method returns true.
+   * sup and this method returns true.
    *
    * @param sub class to test for being a subtype
    * @param sup class to test for being a supertype
