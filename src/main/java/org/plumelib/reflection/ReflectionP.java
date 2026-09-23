f09a09fbac9008f61eb0d0761a1b8d1384d7b5be
Michael Ernst <mernst@cs.washington.edu>
2026-09-23 14:26:34 -0700

    Rename purity warning suppressions to the shorter keys (#545)

--- src/main/java/org/plumelib/reflection/ReflectionP.java
+++ src/main/java/org/plumelib/reflection/ReflectionP.java
@@ -49,7 +49,7 @@ public final class ReflectionP {
    * @return true iff sub is a subtype of sup
    */
   @SuppressWarnings({
-    "allcheckers:purity.not.deterministic.call", // getInterfaces() is used as a set
+    "allcheckers:purity.call", // getInterfaces() is used as a set
     "allcheckers:method.guarantee.violated" // getInterfaces() is used as a set
   })
   @Pure
